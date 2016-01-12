8eb0008dcca501b747ea0f2ceb3ac3cf33a30b55
Nicholas Chum <nicholaschum@gmail.com>
2016-01-12 00:55:42 -0500

    Completely rebase app to be buildable by source (Fresh start)
    Fork this repo at this stage to fork Paperboard without Paperboard
    features.

---
+++ app/src/main/java/bliss/about/MainActivity.java
@@ -0,0 +1,16 @@
+package bliss.about;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+
+public class MainActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Intent intent = new Intent(MainActivity.this, bliss.about.activities.MainActivity.class);
+        startActivity(intent);
+    }
+}
